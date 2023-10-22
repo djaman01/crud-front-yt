a10b9582009e1d2f01e195da262e33a9206aaf46
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-10-22 11:31:37 +0100

    avancée APP.js avec axios.get et apparition task browser

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import { List } from './components/List';
+import List from './components/List';
+import axios from "axios"
+import { baseURL } from './utils/constant';
 
 
 //rafce = shortcut for React arrow function component
 const App = () => {
 
   const [input, setInput] = useState("");
-  const [task, setTasks] = useState("");
+  const [tasks, setTasks] = useState([]);//Will handle all the data, and it is an array
+  const [updateUI, setUpdateUI] = useState(false); //Pour faire apparaitre la task qu'on a ajouté dans le browser
 
   const handleInput = (e) => setInput(e.target.value);
 
-  useEffect(()=> {
-    
-  }, [])
+  useEffect(() => {
+    axios.get(`${baseURL}/get `)
+      .then((res) => {
+        console.log(res.data);
+        setTasks(res.data) //la data ecrite devient la valeur de la state variable tasks
+      })
+  }, [updateUI])//When the state variable updateUI will change, then the useEffect will re-rendre, and get the data
+
+  //on donne à la propriété task de l'objet dans la base de donnée, la valeur de la "state variable input" qui est la valeur de l'input
+  const addTask = () => {
+    axios.post(`${baseURL}/save`, { task: input }).then((res) => {
+      console.log(res.data);
+      setInput("");
+      setUpdateUI((prevState)=> !prevState) //Pour montrer la task qu'on a ajouter dans le browser automatiquement, sans reload car: a chaque fois qu'on va addTask ca va toggle la valeur de updateUI entre false et true, et donc re-render le useEffect qui donne la valeur a task variable et donc va apparaitre dans le browser car on map dessus
+    })
+  }
 
   return (
     <main>
@@ -21,13 +37,21 @@ const App = () => {
       <div className="input_holder">
 
         <input type="text" value={input} onChange={handleInput} />
-        <button type='submit'> Add Task </button>
+
+        {/* Quand on clique sur le bouton ça appelle la function addTask, qui donne la valeur de l'input à la property task dans la base de donnée */}
+        <button type='submit' onClick={addTask}> Add Task </button>
 
       </div>
 
+      {/* We will map through the tasks state variable which is an Array of elements */}
       <ul>
-        {/* Component qu'on a crée plus tard */}
-        <List task="Something" />
+        {tasks.map((task) => (  //On va passer des valeurs au props de <List /> 
+          <List
+            key={task._id}
+            id={task._id}
+            task={task.task} //task={task.task}= attribut task = element task et SA property task= valeur de l'input qui va donc s'afficher dans le browser quand on va addTask
+            setUpdateUI={setUpdateUI} />
+        ))}
       </ul>
 
 
--- src/components/List.js
+++ src/components/List.js
@@ -3,7 +3,7 @@ import React from 'react'
 import {BsTrash} from "react-icons/bs";
 import {BiEditAlt} from "react-icons/bi";
 
-export const List = ({id, task,setUpdateUI,  updateMode, }) => {
+const List = ({id, task, setUpdateUI,  updateMode, }) => {
   return (
     <li>
       {task}
@@ -14,3 +14,5 @@ export const List = ({id, task,setUpdateUI,  updateMode, }) => {
     </li>
   )
 };
+
+export default List;
